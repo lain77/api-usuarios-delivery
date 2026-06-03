e8226931b775b8284ac6a9336676c738c1a2d66d
Oxcart <victorsamuel2006@gmail.com>
2026-06-03 10:53:12 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
+
+echo "Carregando segredos do Infisical..."
+export $(infisical secrets --projectId="$INFISICAL_PROJECT_ID" --env=prod --plain --silent | xargs)
+
 echo "Aplicando migrations..."
 npx prisma migrate deploy
+
 echo "Iniciando microsserviço..."
 exec node src/index.js
