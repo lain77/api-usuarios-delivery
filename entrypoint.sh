210aa1c2138b77c8f39bd7583c56cdbc5a748ef0
Oxcart <victorsamuel2006@gmail.com>
2026-06-17 11:13:51 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+# A CLI do Infisical lê automaticamente as credenciais destas variáveis de ambiente:
+#   INFISICAL_UNIVERSAL_AUTH_CLIENT_ID
+#   INFISICAL_UNIVERSAL_AUTH_CLIENT_SECRET
+# (injetadas pelo docker-compose a partir do .env, que NÃO é commitado)
+# Por isso não é necessário um "infisical login" separado.
+
 echo "Aplicando migrations com segredos do Infisical..."
 infisical run \
   --projectId="$INFISICAL_PROJECT_ID" \
